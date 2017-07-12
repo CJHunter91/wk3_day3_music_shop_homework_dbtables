4690446742a41ff44c943e8ffddfc5290233c1eb
Chris Hunter <orkem2@gmail.com>
2017-07-12 16:52:52 +0100

    Added update function in Artist class

--- db/console.rb
+++ db/console.rb
@@ -10,7 +10,8 @@ album2= Album.new({'title' => 'Godsmack', 'genre' => 'metal', 'artist_id' => art
 
 album1.save
 album2.save
-
+artist1.name = "Chris"
+artist1.update
 Artist.list_all
 Album.list_all
 artist1.list_albums
--- models/artist.rb
+++ models/artist.rb
@@ -5,28 +5,34 @@ require_relative('album')
 class Artist
 
   attr_reader :id
+  attr_accessor :name
 
-def initialize(origin)
-  @id = origin['id'] if origin['id']
-  @name = origin['name']
-end
+  def initialize(origin)
+    @id = origin['id'] if origin['id']
+    @name = origin['name']
+  end
 
-def save
-  sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;"
-  @id = SqlRunner.run(sql)[0]['id'].to_i
-end
+  def save
+    sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;"
+    @id = SqlRunner.run(sql)[0]['id'].to_i
+  end
 
-def self.list_all
-  sql = "SELECT * FROM artists;"
-  artists = SqlRunner.run(sql)
-  artists.map {|artist| Artist.new(artist)}
-end
+  def update
+    sql = "UPDATE artists SET name = '#{@name}'"
+    SqlRunner.run(sql)
+  end
 
-def list_albums
-  sql = "SELECT * FROM albums WHERE artist_id = #{@id};"
-  albums = SqlRunner.run(sql)
-  return albums.map{|album| Album.new(album)}
-end
+  def self.list_all
+    sql = "SELECT * FROM artists;"
+    artists = SqlRunner.run(sql)
+    artists.map {|artist| Artist.new(artist)}
+  end
+
+  def list_albums
+    sql = "SELECT * FROM albums WHERE artist_id = #{@id};"
+    albums = SqlRunner.run(sql)
+    return albums.map{|album| Album.new(album)}
+  end
 
 
 end
